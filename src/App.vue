7809cf01cf54f0ade0d1d646a8238e4188db4d96
luoshushu <852835916@qq.com>
2018-04-08 11:01:21 +0800

    修改预览后的背景色及头像

--- src/App.vue
+++ src/App.vue
@@ -16,220 +16,239 @@
 </template>
 
 <script>
-  import Cephalosome from "./components/Cephalosome";
-  import EditBox from "./components/EditBox";
-  import ShowBox from "./components/ShowBox";
-  import Login from "./components/Login";
-  import SignUp from "./components/SignUp";
-  import html2canvas from "html2canvas";
-  import jsPDF from "jsPDF";
-  export default {
-    data() {
-      return {
-        previewMode: false,
-        loginMode: false,
-        signUpMode: false,
-        resume: {
-          // 声明数据
-          profile: {
-            name: "",
-            city: "",
-            bitrh: ""
-          },
-          //声明工作经历数据
-          work: [{
+import Cephalosome from "./components/Cephalosome";
+import EditBox from "./components/EditBox";
+import ShowBox from "./components/ShowBox";
+import Login from "./components/Login";
+import SignUp from "./components/SignUp";
+import html2canvas from "html2canvas";
+import jsPDF from "jsPDF";
+export default {
+  data() {
+    return {
+      previewMode: false,
+      loginMode: false,
+      signUpMode: false,
+      resume: {
+        // 声明数据
+        profile: {
+          name: "",
+          city: "",
+          bitrh: ""
+        },
+        //声明工作经历数据
+        work: [
+          {
             company: "",
             date: "",
             content: ""
-          }],
-          // 声明教育信息
-          education: [{
+          }
+        ],
+        // 声明教育信息
+        education: [
+          {
             school: "",
             time: "",
             degree: ""
-          }],
-          //声明项目经历
-          project: [{
+          }
+        ],
+        //声明项目经历
+        project: [
+          {
             name: "",
             introduce: "",
             site: ""
-          }],
-          //声明获奖情况数组
-          award: [{
+          }
+        ],
+        //声明获奖情况数组
+        award: [
+          {
             name: "",
             time: ""
-          }],
-          //声明联系方式
-          contact: [{
+          }
+        ],
+        //声明联系方式
+        contact: [
+          {
             mobile: "",
             WeChat: "",
             email: ""
-          }]
-        }
-      };
+          }
+        ]
+      }
+    };
+  },
+  methods: {
+    exitButton() {
+      this.previewMode = false;
+      document.body.style.setProperty("height", "100%", "important");
+      document.body.style.setProperty("background", "#fff");
     },
-    methods: {
-      exitButton() {
-        this.previewMode = false;
-        document.body.style.setProperty("height", "100%", "important");
-      },
-      exitX() {
-        this.loginMode = false;
-        this.signUpMode = false;
-      },
-      preview() {
-        this.previewMode = true;
-        this.signUpMode = false;
-        this.loginMode = false;
-        document.body.style.setProperty("height", "auto", "important");
-      },
-      login() {
-        // this.previewMode = true;
-        this.loginMode = true;
-        this.signUpMode = false;
-      },
-      signUp() {
-        this.signUpMode = true;
-        this.loginMode = false;
-      },
-      hideModal() {
-        this.signUpMode = false;
-        this.loginMode = false;
-      },
-      hide() {
-        this.signUpMode = false;
-        this.loginMode = false;
-      },
-      download() {
-        //  window.print()
-   
-        html2canvas(document.querySelector(".showBox")).then(canvas => {
-    //     var opts = {useCORS: true};
-
-    // html2canvas(element, opts);
-          let imgData = canvas.toDataURL("image/JPEG");
-          let imgWidth = 210;
-          let pageHeight = 295;
-          let imgHeight = canvas.height * imgWidth / canvas.width;
-          let heightLeft = imgHeight;
-          let doc = new jsPDF("p", "mm");
-          let position = 0;
+    exitX() {
+      this.loginMode = false;
+      this.signUpMode = false;
+    },
+    preview() {
+      this.previewMode = true;
+      this.signUpMode = false;
+      this.loginMode = false;
+      document.body.style.setProperty("height", "auto", "important");
+      document.body.style.setProperty("background", "#F2F6FC");
+    },
+    login() {
+      // this.previewMode = true;
+      this.loginMode = true;
+      this.signUpMode = false;
+    },
+    signUp() {
+      this.signUpMode = true;
+      this.loginMode = false;
+    },
+    hideModal() {
+      this.signUpMode = false;
+      this.loginMode = false;
+    },
+    hide() {
+      this.signUpMode = false;
+      this.loginMode = false;
+    },
+    download() {
+      // window.print()
+      //  ,useCORS=true
+      //   html2canvas(document.body).then(function(canvas) {
+      //     document.body.appendChild(canvas);
+      // });
+      // /*
+      var opts = { useCORS: true };
+      html2canvas(document.querySelector(".showBox")).then(canvas => {
+        let imgData = canvas.toDataURL("image/JPEG");
+        let imgWidth = 210;
+        let pageHeight = 295;
+        let imgHeight = canvas.height * imgWidth / canvas.width;
+        let heightLeft = imgHeight;
+        let doc = new jsPDF("p", "mm");
+        let position = 0;
+        doc.addImage(imgData, "JPEG", 0, position, imgWidth, imgHeight);
+        heightLeft -= pageHeight;
+        while (heightLeft > 0) {
+          position = heightLeft - imgHeight;
+          doc.addPage();
           doc.addImage(imgData, "JPEG", 0, position, imgWidth, imgHeight);
           heightLeft -= pageHeight;
-          while (heightLeft > 0) {
-            position = heightLeft - imgHeight;
-            doc.addPage();
-            doc.addImage(imgData, "JPEG", 0, position, imgWidth, imgHeight);
-            heightLeft -= pageHeight;
-          }
-          doc.save("Resume.pdf");
-        });
-      }
-    },
-    components: {
-      Cephalosome,
-      EditBox,
-      ShowBox,
-      Login,
-      SignUp
+        }
+        doc.save("Resume.pdf");
+      });
+      // */
     }
-  };
+  },
+  components: {
+    Cephalosome,
+    EditBox,
+    ShowBox,
+    Login,
+    SignUp
+  }
+};
 </script>
 
 <style lang="scss">
-  html,
-  body,
-  #app {
-    height: 100%;
-    overflow: visible;
+html,
+body,
+#app {
+  height: 100%;
+  overflow: visible;
+  position: relative;
+}
+.icon {
+  width: 2em;
+  height: 2em;
+  vertical-align: -0.15em;
+  fill: #fff;
+  overflow: hidden;
+}
+#app {
+  font-family: "Franklin Gothic Medium", "Arial Narrow", Arial, sans-serif; // height: 100vh;
+  display: flex;
+  flex-direction: column;
+  .cephalosome {
     position: relative;
-  }
-  .icon {
-    width: 2em;
-    height: 2em;
-    vertical-align: -0.15em;
-    fill: #fff;
+    box-shadow: 0px 0px 3px 2px hsla(0, 0, 0, 0.3);
+    z-index: 1;
     overflow: hidden;
   }
-  #app {
-    font-family: "Franklin Gothic Medium", "Arial Narrow", Arial, sans-serif; // height: 100vh;
+  main {
     display: flex;
-    flex-direction: column;
-    .cephalosome {
-      position: relative;
-      box-shadow: 0px 0px 3px 2px hsla(0, 0, 0, 0.3);
-      z-index: 1; // background: #1682fb;
+    flex: 1;
+    // background: #fff;
+    background: #f2f6fc;
+    > .editBox {
+      background: #fff;
+      width: 36em;
+      margin: 16px 8px 16px 16px;
+      box-shadow: 0px 0px 1px 2px hsla(0, 0, 0, 0.1);
+      border-radius: 5px;
       overflow: hidden;
     }
-    main {
-      display: flex;
+    > .showBox {
+      background: #fff;
       flex: 1;
-      background: #f2f6fc;
-      >.editBox {
-        background: #fff;
-        width: 36em;
-        margin: 16px 8px 16px 16px;
-        box-shadow: 0px 0px 1px 2px hsla(0, 0, 0, 0.1);
-        border-radius: 5px;
-        overflow: hidden;
-      }
-      >.showBox {
-        background: #fff;
-        flex: 1;
-        margin: 16px 16px 16px 8px;
-        box-shadow: 0px 0px 1px 2px hsla(0, 0, 0, 0.1);
-        border-radius: 5px;
-        overflow: auto;
-      }
+      margin: 16px 16px 16px 8px;
+      box-shadow: 0px 0px 1px 2px hsla(0, 0, 0, 0.1);
+      border-radius: 5px;
+      overflow: auto;
     }
   }
-  .previewMode>#Cephalosome {
-    display: none;
-  }
-  .previewMode #EditBox {
-    display: none;
-  }
-  .previewMode #ShowBox {
-    max-width: 65vw;
-    margin: 16px auto !important;
-  }
-  .previewMode html,
-  body,
-  #app {
-    height: none;
-    overflow: visible;
-  }
-  #exitButton,
-  #download {
-    display: none;
-  }
-  .previewMode #exitButton {
-    display: inline-block;
-    position: fixed;
-    top: 16px;
-    right: 16px;
-  }
-  .previewMode #download {
-    display: inline-block;
-    position: fixed;
-    top: 64px;
-    right: 16px;
-  }
-  #login,
-  #signUp {
-    display: none;
-    background: #fff;
-    border-radius: 16px;
-    box-shadow: 0px 0px 2px 2px hsla(0, 0, 0, 0.3);
-    width: 400px;
-    position: absolute;
-    top: 50%;
-    left: 50%;
-    transform: translate(-50%, -50%);
-    z-index: 2;
-  }
-  #app>.loginMode,
-  #app>.signUpMode {
-    display: block;
-  }
+}
+.previewMode main {
+  background: #fff;
+}
+.previewMode > #Cephalosome {
+  display: none;
+}
+.previewMode #EditBox {
+  display: none;
+}
+.previewMode #ShowBox {
+  max-width: 65vw;
+  margin: 16px auto !important;
+}
+.previewMode html,
+body,
+#app {
+  height: none;
+  overflow: visible;
+}
+#exitButton,
+#download {
+  display: none;
+}
+.previewMode #exitButton {
+  display: inline-block;
+  position: fixed;
+  top: 16px;
+  right: 16px;
+}
+.previewMode #download {
+  display: inline-block;
+  position: fixed;
+  top: 64px;
+  right: 16px;
+}
+#login,
+#signUp {
+  display: none;
+  background: #fff;
+  border-radius: 16px;
+  box-shadow: 0px 0px 2px 2px hsla(0, 0, 0, 0.3);
+  width: 400px;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  z-index: 2;
+}
+#app > .loginMode,
+#app > .signUpMode {
+  display: block;
+}
 </style>
--- src/components/Imges.vue
+++ src/components/Imges.vue
@@ -3,101 +3,100 @@
     <div class="file">
       <input type="file" @change="onFileChange" />
       <p id="pp">{{percentage + '%'}}</p>
-      <img v-bind:src="imgUrl" alt="上传120 x 130图片">
+      <img v-bind:src="imgUrl" alt="上传120 x 130图片"  >
     </div>
   </div>
 </template>
 
 <style lang="scss">
-  .file {
-    // width: 120px;
+.file {
+
+  // width: 120px;
+  height: 130px;
+  position: relative;
+  display: inline-block;
+  background: #d0eeff;
+  border: 1px solid #99d3f5;
+  border-radius: 4px; // padding: 4px 12px;
+  overflow: hidden;
+  color: #1e88c7;
+  text-decoration: none;
+  text-indent: 0;
+  line-height: 20px;
+  > input {
+    position: absolute;
+    font-size: 100px;
+    right: 0;
+    top: 0;
+    opacity: 0;
+  }
+  > img {
+    width: 120px;
     height: 130px;
-    position: relative;
-    display: inline-block;
-    background: #D0EEFF;
-    border: 1px solid #99D3F5;
-    border-radius: 4px; // padding: 4px 12px;
-    overflow: hidden;
-    color: #1E88C7;
-    text-decoration: none;
-    text-indent: 0;
-    line-height: 20px;
-    >input {
-      position: absolute;
-      font-size: 100px;
-      right: 0;
-      top: 0;
-      opacity: 0;
-    }
-    >img {
-      width: 120px;
-      height: 130px;
-      vertical-align: middle;
-    }
-    >p {
-      text-align: center;
-    }
+    vertical-align: middle;
   }
-  .file:hover {
-    background: #AADFFD;
-    border-color: #78C3F3;
-    color: #004974;
-    text-decoration: none;
+  > p {
+    text-align: center;
   }
+}
+.file:hover {
+  background: #aadffd;
+  border-color: #78c3f3;
+  color: #004974;
+  text-decoration: none;
+}
 </style>
 
 <script>
-  import AV from "leancloud-storage"; //注意一要引入AV
-  export default {
-    data() {
+import AV from "leancloud-storage"; //注意一要引入AV
+export default {
+  data() {
+    return {
+      percentage: 0,
+      imgUrl: "../src/assets/picture.png"
+    };
+  },
+  methods: {
+    plan() {
       return {
-        percentage: 0,
-        imgUrl: ''
-      }
+        onprogress: e => {
+          this.percentage = Math.ceil(e.percent);
+
+          console.log(Math.ceil(e.percent)); //上传后进度条
+        }
+      };
     },
-    methods: {
-      plan() {
-        return {
-          onprogress: e => {
-            this.percentage = Math.ceil(e.percent)
-            if (this.percentage === 100) {
-              document.getElementById("pp").style.display = "none"
+    okImg(file) {
+      this.imgUrl = file.url()
+      console.log(file.url());
+      // this.imgUrl = file.thumbnailURL(120, 600);
+      // console.log(this.imgUrl);
+    },
+    onFileChange(e) {
+      let imgLength = e.target.files.length;
+      if (imgLength > 0) {
+        var localFile = e.target.files[0];
+        if (localFile.type === "image/png" || localFile.type === "image/jpg") {
+          let name = localFile.name;
+          let file = new AV.File(name, localFile);
+          file.save(this.plan()).then(file => {
+              // 文件保存成功
+              document.getElementById("pp").style.display = "none";
+
+              this.okImg(file);
+            },
+            function(error) {
+              // 异常处理
+              console.error(error);
             }
-            console.log(Math.ceil(e.percent)); //上传后进度条
-          }
-        };
-      },
-      okImg(file) {
-        this.imgUrl = file.thumbnailURL(120, 600);
-        console.log(this.imgUrl);
-      },
-      onFileChange(e) {
-        let imgLength = e.target.files.length;
-        if (imgLength > 0) {
-          var localFile = e.target.files[0];
-          console.log(localFile.type)
-          if (localFile.type === 'image/png' || localFile.type === 'image/jpg') {
-            console.log(localFile.type)
-            let name = localFile.name;
-            let file = new AV.File(name, localFile);
-            file.save(this.plan()).then(
-              (file) => {
-                // 文件保存成功
-                // console.log(file.url());
-                this.okImg(file)
-              },
-              function(error) {
-                // 异常处理
-                console.error(error);
-              }
-            );
-          } else {
-            console.log("格式错误了！请用jpg/png格式上传")
-          }
+          );
+        } else {
+         alert("格式错误了！请用jpg/png格式上传");
         }
       }
     }
-  };
+  }
+};
 </script>
 
 
